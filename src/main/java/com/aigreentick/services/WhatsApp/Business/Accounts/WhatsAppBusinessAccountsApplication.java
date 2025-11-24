d8f78a4c564575b6ee00b82a75134861113d770d
Yuvraj <yuvrajbhinchar00@gmail.com>
2025-11-24 15:47:26 +0530

    first commit

---
+++ src/main/java/com/aigreentick/services/WhatsApp/Business/Accounts/WhatsAppBusinessAccountsApplication.java
@@ -0,0 +1,13 @@
+package com.aigreentick.services.WhatsApp.Business.Accounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsAppBusinessAccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatsAppBusinessAccountsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aigreentick/services/WhatsApp/Business/Accounts/WhatsAppBusinessAccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aigreentick.services.WhatsApp.Business.Accounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsAppBusinessAccountsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
